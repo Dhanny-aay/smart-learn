514140e665089d42647c00d38d0d438f8aa0a7ff
Dhanny-aay <danielakintolu@gmail.com>
2023-02-06 18:35:21 +0100

    finished mobile design

--- src/home.js
+++ src/home.js
@@ -8,6 +8,10 @@ import courses from './courses.png';
 import go from './go.png';
 import play from './play.png';
 import dots from './3dots.png';
+import insi from './ins1.png';
+import insii from './ins2.png';
+import insiii from './ins3.png';
+import insiv from './ins4.png';
 
 const home = () => {
     return ( 
@@ -90,47 +94,97 @@ const home = () => {
                             </select>
                         </span>
 
-                        <div className=' mt-3 w-full bg-[#f1f1f1] py-7  rounded-[15px] flex flex-col space-y-[35px]'>
-                            <div className=' flex items-center justify-between hover:bg-white hover:rounded-[25px] h-[79px] px-3'>
+                        <div className=' mt-3 w-full bg-[#f1f1f1] py-7  rounded-[15px] flex flex-col space-y-[20px]'>
+                            <div className=' flex items-center justify-between hover:bg-white hover:rounded-[25px] h-[79px] px-3 hover:shadow transition-all delay-200'>
                                 <span className=' h-[40px] w-[40px] rounded-[15px] bg-[#ffcf2d]'></span>
                                 <span className=' flex flex-col font-bold font-sans text-[12px]'>
-                                    <p classname=' font-bold font-sans text-[12px]'>Web Design</p>
-                                    <p classname=' font-medium font-sans text-[10px]'>20+ Courses</p>
+                                    <p className=' font-bold font-sans text-[12px]'>Web Design</p>
+                                    <p className=' font-medium font-sans text-[10px]'>20+ Courses</p>
                                 </span>
-                                <button className=' px-4 py-2 bg-[#ffcf2d] font-sans font-medium text-xs bg-opacity-20 text-center hover:text-white hover:bg-opacity-0 rounded-[20px]'>View Course</button>
+                                <button className=' px-4 py-2 bg-[#ffcf2d] font-sans font-medium text-xs bg-opacity-20 text-center hover:text-white hover:bg-opacity-100 rounded-[20px] transition-all'>View Course</button>
                                 <img src={ dots } className='' alt="" />
                             </div>
 
-                            <div className=' flex items-center justify-between hover:bg-white hover:rounded-[25px] h-[79px] px-3'>
+                            <div className=' flex items-center justify-between hover:bg-white hover:rounded-[25px] h-[79px] px-3 hover:shadow transition-all delay-200'>
                                 <span className=' h-[40px] w-[40px] rounded-[15px] bg-[#2708a0]'></span>
                                 <span className=' flex flex-col font-bold font-sans text-[12px]'>
-                                    <p classname=' font-bold font-sans text-[12px]'>UI/UX Design</p>
-                                    <p classname=' font-medium font-sans text-[10px]'>30+ Courses</p>
+                                    <p className=' font-bold font-sans text-[12px]'>UI/UX Design</p>
+                                    <p className=' font-medium font-sans text-[10px]'>30+ Courses</p>
                                 </span>
-                                <button className=' px-4 py-2 bg-[#2708a0] font-sans font-medium text-xs bg-opacity-20 text-center hover:text-white hover:bg-opacity-0 rounded-[20px]'>View Course</button>
+                                <button className=' px-4 py-2 bg-[#2708a0] font-sans font-medium text-xs bg-opacity-20 text-center hover:text-white hover:bg-opacity-100 rounded-[20px] transition-all'>View Course</button>
                                 <img src={ dots } className='' alt="" />
                             </div>
 
-                            <div className=' flex items-center justify-between hover:bg-white hover:rounded-[25px] h-[79px] px-3'>
+                            <div className=' flex items-center justify-between hover:bg-white hover:rounded-[25px] h-[79px] px-3 hover:shadow transition-all delay-200'>
                                 <span className=' h-[40px] w-[40px] rounded-[15px] bg-[#fe5244]'></span>
                                 <span className=' flex flex-col font-bold font-sans text-[12px]'>
-                                    <p classname=' font-bold font-sans text-[12px]'>Web Design</p>
-                                    <p classname=' font-medium font-sans text-[10px]'>40+ Courses</p>
+                                    <p className=' font-bold font-sans text-[12px]'>Web Design</p>
+                                    <p className=' font-medium font-sans text-[10px]'>40+ Courses</p>
                                 </span>
-                                <button className=' px-4 py-2 bg-[#fe5244] font-sans font-medium text-xs bg-opacity-20 text-center hover:text-white hover:bg-opacity-0 rounded-[20px]'>View Course</button>
+                                <button className=' px-4 py-2 bg-[#fe5244] font-sans font-medium text-xs bg-opacity-20 text-center hover:text-white hover:bg-opacity-100 rounded-[20px] transition-all'>View Course</button>
                                 <img src={ dots } className='' alt="" />
                             </div>
 
-                            <div className=' flex items-center justify-between hover:bg-white hover:rounded-[25px] h-[79px] px-3'>
+                            <div className=' flex items-center justify-between hover:bg-white hover:rounded-[25px] h-[79px] px-3 hover:shadow transition-all delay-200'>
                                 <span className=' h-[40px] w-[40px] rounded-[15px] bg-[#3fb1b5]'></span>
                                 <span className=' flex flex-col font-bold font-sans text-[12px]'>
-                                    <p classname=' font-bold font-sans text-[12px]'>UI/UX Design</p>
-                                    <p classname=' font-medium font-sans text-[10px]'>80+ Courses</p>
+                                    <p className=' font-bold font-sans text-[12px]'>UI/UX Design</p>
+                                    <p className=' font-medium font-sans text-[10px]'>80+ Courses</p>
                                 </span>
-                                <button className=' px-4 py-2 bg-[#3fb1b5] font-sans font-medium text-xs bg-opacity-20 text-center hover:text-white hover:bg-opacity-0 rounded-[20px]'>View Course</button>
+                                <button className=' px-4 py-2 bg-[#3fb1b5] font-sans font-medium text-xs bg-opacity-20 text-center hover:text-white hover:bg-opacity-100 rounded-[20px] transition-all '>View Course</button>
                                 <img src={ dots } className='' alt="" />
                             </div>
 
+                        </div>
+                    </div>
+                </div>
+
+                <div className=' mt-5'>
+                    <span className=' flex flex-row justify-between'>
+                        <p className=' font-sans font-medium text-[18px] text-[#160e4d]'>Best Instructors</p>
+                        <p className=' font-sans font-normal text-[14px] underline text-[#160e4d]'>See All</p>
+                    </span>
+
+                    <div className=' bg-[#f1f1f1] rounded-[15px] py-5 space-y-[25px]'>
+                        <div className=' flex justify-between px-5 items-center'>
+                            <div className=' flex flex-row items-center justify-between space-x-2'>
+                                <img src={ insi } className='' alt="" />
+                                <span className=' '>
+                                    <p className=' font-sans font-medium text-base '>Devon Lane</p>
+                                    <p className=' font-sans font-medium text-xs'>5 Design Courses</p>
+                                </span>
+                            </div>
+                            <button className=' border border-[rgba(22,14,77,0.4)] rounded-[10px] px-3 h-[30px] text-[rgba(0,0,0,0.7)] font-normal text-[10px] font-sans hover:bg-[#2708a0] transition-all hover:text-white'>Courses</button>
+                        </div>
+                        <div className=' flex justify-between px-5 items-center'>
+                            <div className=' flex flex-row items-center justify-between space-x-2'>
+                                <img src={ insii } className='' alt="" />
+                                <span className=' '>
+                                    <p className=' font-sans font-medium text-base '>Albert Flores</p>
+                                    <p className=' font-sans font-medium text-xs'>5 Design Courses</p>
+                                </span>
+                            </div>
+                            <button className=' border border-[rgba(22,14,77,0.4)] rounded-[10px] px-3 h-[30px] text-[rgba(0,0,0,0.7)] font-normal text-[10px] font-sans hover:bg-[#2708a0] transition-all hover:text-white'>Courses</button>
+                        </div>
+                        <div className=' flex justify-between px-5 items-center'>
+                            <div className=' flex flex-row items-center justify-between space-x-2'>
+                                <img src={ insiii } className='' alt="" />
+                                <span className=' '>
+                                    <p className=' font-sans font-medium text-base '>Jane Cooper</p>
+                                    <p className=' font-sans font-medium text-xs'>5 Design Courses</p>
+                                </span>
+                            </div>
+                            <button className=' border border-[rgba(22,14,77,0.4)] rounded-[10px] px-3 h-[30px] text-[rgba(0,0,0,0.7)] font-normal text-[10px] font-sans hover:bg-[#2708a0] transition-all hover:text-white'>Courses</button>
+                        </div>
+                        <div className=' flex justify-between px-5 items-center'>
+                            <div className=' flex flex-row items-center justify-between space-x-2'>
+                                <img src={ insiv } className='' alt="" />
+                                <span className=' '>
+                                    <p className=' font-sans font-medium text-base '>Cody Fisher</p>
+                                    <p className=' font-sans font-medium text-xs'>5 Design Courses</p>
+                                </span>
+                            </div>
+                            <button className=' border border-[rgba(22,14,77,0.4)] rounded-[10px] px-3 h-[30px] text-[rgba(0,0,0,0.7)] font-normal text-[10px] font-sans hover:bg-[#2708a0] transition-all hover:text-white'>Courses</button>
                         </div>
 
                     </div>
@@ -138,7 +192,7 @@ const home = () => {
                 </div>
 
             </div>
-                
+
             </div>
         </div>
      );
