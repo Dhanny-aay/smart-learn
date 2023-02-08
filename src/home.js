590863684ca3c26260963179a16432bbb97e1780
Dhanny-aay <danielakintolu@gmail.com>
2023-02-08 23:02:36 +0100

    starte work on making it responsive for desktop

--- src/home.js
+++ src/home.js
@@ -22,41 +22,53 @@ import menutab from './menutab.png';
 import set from './set.png';
 import mail from './mail.png';
 import noti from './noti.png';
+import trate from './trate.png';
 
 const home = () => {
     return ( 
         
         <div className="">
-                <div className='bg-[#f1f1f1] hidden z-50 md:block fixed md:w-[85px] h-full left-0 top-0 px-5 pt-14'>
+                <div className='bg-[#f1f1f1] hidden z-50 md:block fixed md:w-[85px] lg:w-[265px] h-full left-0 top-0 px-5 md:pt-14 lg:pt-7'>
                     <div className=' flex flex-col items-center '>
-                        <span className=''>
+                        <span className=' flex flex-row items-center lg:space-x-1'>
                             <img src={ logotab } className='' alt="" />
+                            <p className=' hidden lg:block font-sans text-[#160e4d] text-[20px] font-semibold'>Smart Learning</p>
                         </span>
-                        <span className=' flex flex-col items-center mt-14 space-y-8'>
-                            <button className=' p-3 hover:bg-white hover:rounded-[10px] hover:shadow transition-all'>
-                                <img src={ menutab } className='' alt="" />
+                        <span className=' flex flex-col items-center md:mt-14 lg:mt-5 md:space-y-8 lg:space-y-2 lg:items-start'>
+                            <button className=' p-3 hover:bg-white hover:rounded-[10px] lg:hover:rounded-[30px] lg:px-5 hover:shadow transition-all flex flex-row items-center lg:space-x-2'>
+                                <img src={ menutab } className=' lg:w-5' alt="" />
+                                <p className='hidden lg:block font-sans text-[16px] font-semibold text-[#160e4d]'>Dashboard</p>
                             </button>
-                            <button className=' p-3 hover:bg-white hover:rounded-[10px] hover:shadow transition-all'>
-                                <img src={ marks } className='' alt="" />
+                            <button className=' p-3 hover:bg-white hover:rounded-[10px] hover:shadow transition-all flex flex-row items-center lg:space-x-2 lg:hover:rounded-[30px] lg:px-5'>
+                                <img src={ marks } className=' lg:w-5' alt="" />
+                                <p className='hidden lg:block font-sans text-[16px] font-semibold text-[#160e4d]'>My Class</p>
                             </button>
-                            <button className=' p-3 hover:bg-white hover:rounded-[10px] hover:shadow transition-all'>
-                                <img src={ bio } className='' alt="" />
+                            <button className=' p-3 hover:bg-white hover:rounded-[10px] hover:shadow transition-all flex flex-row items-center lg:space-x-2 lg:hover:rounded-[30px] lg:px-5'>
+                                <img src={ bio } className=' lg:w-5' alt="" />
+                                <p className='hidden lg:block font-sans text-[16px] font-semibold text-[#160e4d]'>Instructors</p>
                             </button>
-                            <button className=' p-3 hover:bg-white hover:rounded-[10px] hover:shadow transition-all'>
-                                <img src={ col } className='' alt="" />
+                            <button className=' p-3 hover:bg-white hover:rounded-[10px] hover:shadow transition-all flex flex-row items-center lg:space-x-2 lg:hover:rounded-[30px] lg:px-5'>
+                                <img src={ col } className=' lg:w-5' alt="" />
+                                <p className='hidden lg:block font-sans text-[16px] font-semibold text-[#160e4d]'>Courses</p>
                             </button>
-                            <button className=' p-3 hover:bg-white hover:rounded-[10px] hover:shadow transition-all'>
-                                <img src={ chat } className='' alt="" />
+                            <button className=' p-3 hover:bg-white hover:rounded-[10px] hover:shadow transition-all flex flex-row items-center lg:space-x-2 lg:hover:rounded-[30px] lg:px-5'>
+                                <img src={ chat } className=' lg:w-5' alt="" />
+                                <p className='hidden lg:block font-sans text-[16px] font-semibold text-[#160e4d]'>Group chats</p>
                             </button>
-                            <button className=' md:absolute bottom-4 border-t border-[#160e4d] py-5 px-3 hover:bg-white hover:rounded-[10px] hover:shadow transition-all'>
-                                <img src={ set } className='' alt="" />
+                            <button className=' md:absolute lg:static bottom-4 md:border-t lg:border-none border-[#160e4d] md:py-5 lg:py-3 px-3 hover:bg-white hover:rounded-[10px] hover:shadow transition-all flex flex-row items-center lg:space-x-2 lg:hover:rounded-[30px] lg:px-5'>
+                                <img src={ set } className=' lg:w-5' alt="" />
+                                <p className='hidden lg:block font-sans text-[16px] font-semibold text-[#160e4d]'>Settings</p>
                             </button>
 
                         </span>
+                        <div className='hidden lg:block lg:absolute bottom-6 mx-5 h-[250px] lg:mt-[5%] w-[220px] bg-[#8eb8e5] rounded-[30px] relative'>
+                            <img src={ trate } className='w-[190px] absolute left-[15px] -top-[15px]' alt="" />
+                            <button className=' bg-opacity-50 rounded-[15px] px-4 py-2 text-[10px] font-sans font-medium bottom-3 absolute left-[65px] bg-[#fafafa]'>Join Courses</button>
+                        </div>
                     </div>     
 
                 </div>
-            <div className=" navbar py-6 flex justify-between px-4 md:pr-8 md:ml-[110px] md:hidden bg-white">
+            <div className=" navbar py-6 flex justify-between px-4 md:pr-8 md:ml-[110px]  md:hidden bg-white">
                 <span className=" flex justify-center items-center space-x-1">
                     <img src={ logo } className='' alt="" />
                     <p className=" font-sans font-semibold text-[20px] text-[#160e4d]">Smart Learning</p>
@@ -64,7 +76,7 @@ const home = () => {
                 <img src={ menu } className='' alt="" />
             </div>
 
-            <div className=' mt-4 px-4 flex justify-between items-center md:pr-8 md:ml-[110px] md:mt-[40px]'>
+            <div className=' mt-4 px-4 flex justify-between items-center md:pr-8 md:ml-[110px] lg:ml-[275px] md:mt-[40px]'>
                 <input type="search" name="" className=' font-sans h-[40px] text-[rgba(22,14,77,0.5)] px-5 py-2 bg-[#f1f1f1] rounded-[22px]' placeholder='Search for Query' id="" />
 
                 <span className='hidden md:flex flex-row items-center space-x-8'>
@@ -82,7 +94,7 @@ const home = () => {
 
             </div>
 
-            <div className=' mt-[30px] px-5 md:pr-8 md:ml-[110px]'>
+            <div className=' mt-[30px] px-5 md:pr-8 md:ml-[110px] lg:ml-[275px] '>
                 <div>
                     <p className=' font-sans font-medium text-[20px] text-[#160e4d]'>Analytics Overview</p>
                     <div className=' w-full rounded-[22px] md:h-[300px] mt-4 px-5 md:px-11 py-6 md:py-10 bg-[#2708a0] relative flex'>
@@ -114,8 +126,8 @@ const home = () => {
                 
             </div>
 
-            <div className=' mt-[22px] px-5 md:pr-8 md:ml-[110px]'>
-                <div className='flex flex-col'>
+            <div className=' mt-[22px] px-5 md:pr-8 md:ml-[110px] lg:ml-[275px] lg:flex lg:flex-row'>
+                <div className='flex flex-col lg:flex-row'>
                     <div>
                         <p className=' font-sans font-medium text-[20px] text-[#160e4d]'>Current Activity</p>
                         <div className=' w-full rounded-[22px] mt-4 px-5 py-6 bg-[#2708a0] h-[185px]'>
